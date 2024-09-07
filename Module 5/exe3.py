5f86e077c29894733fdd37ae79c76f61fb1cfbcf
Dilmi Tharushika <Dilmi.MerenchiKankanamge@metropolia.fi>
2024-09-08 00:34:38 +0530

    Module 5 exercise 3

---
+++ Module 5/exe3.py
@@ -0,0 +1,10 @@
+number = int(input("Enter a number: "))
+
+if number <=1:
+    print(number," is not a prime number.")
+else:
+    for i in range(2,number):
+        if number % i == 0:
+            print(number, " is not a prime number.")
+            break
+        print(number, " is a prime number.")
